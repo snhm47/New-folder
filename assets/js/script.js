a316cf3a6d41ce724b44db70f63e1b6e4ee3f029
snhm47 <salahhanna86@gmail.com>
2024-10-13 14:46:41 +0300

    complete the sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 'use strict';
 
+
+
 /**
  * navbar toggle
  */
+
 const navbar = document.querySelector("[data-navbar]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
@@ -13,31 +16,34 @@ menuToggleBtn.addEventListener("click", function () {
 });
 
 for (let i = 0; i < navbarLinks.length; i++) {
-    navbarLinks[i].addEventListener("click", function () {
-      navbar.classList.toggle("active");
-      menuToggleBtn.classList.toggle("active");
-    });
-  }
-
-
-
-  /**
-   * header sticky
-   */
-  const header = document.querySelector("[data-header]");
-
-  window.addEventListener("scroll" , function() {
-    if(window.scrollY >= 100){
-        header.classList.add("active");
-    }else{
-        header.classList.remove("active");
-    }
-
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    menuToggleBtn.classList.toggle("active");
   });
+}
 
 
 
-  /**
+/**
+ * header sticky & back to top
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+/**
  * search box toggle
  */
 
@@ -54,3 +60,33 @@ for (let i = 0; i < searchBoxElems.length; i++) {
     document.body.classList.toggle("active");
   });
 }
+
+
+
+/**
+ * move cycle on scroll
+ */
+
+const deliveryBoy = document.querySelector("[data-delivery-boy]");
+
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", function () {
+
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
+
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
+    } else {
+      deliveryBoyMove -= 1;
+    }
+
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
+
+});
